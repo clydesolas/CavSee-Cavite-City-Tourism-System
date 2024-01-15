f0bd4f523f4115f49114bce2baeb8cd76415abd1
clyde <clydesolas1@gmail.com>
2024-01-15 20:33:19 +0800

    push

--- initialize.php
+++ initialize.php
@@ -4,7 +4,7 @@ if(!defined('base_url')) define('base_url','http://localhost/CavSee-Cavite-City-
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"u578342230_cavsee");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"cvsuImus1");
-if(!defined('DB_NAME')) define('DB_NAME',"u578342230_tourism_db");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
+if(!defined('DB_NAME')) define('DB_NAME',"tourism_db");
 ?>
